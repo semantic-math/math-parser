eba7b5d715ef2fa79f9875cb787af00bf74915a1
Kevin Barabash <kevinb@khanacademy.org>
2017-02-05 11:47:23 -0500

    fix issue with numeric factors and implicit multiplication

--- demo.js
+++ demo.js
@@ -4,8 +4,8 @@ var Parser = require("./lib/parser.js");
 // var math = "x+y=1+2-3(4-x(2))";
 // var math = "ab/b/c*(d*e)";
 // var math = "a^b^c";
-// var math = "a 2bold23(3-y(x-1))"
-var math = "x^(-2)"
+var math = "a 2.5bold23(3-y(x-1))"
+// var math = "x^(-2)"
 
 var parser = new Parser();
 var ast = parser.parse(math);
