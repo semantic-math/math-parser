10045163048fc30dfdfa2e354cfe1bfcc92b43e4
Kevin Barabash <kevinb@khanacademy.org>
2017-02-05 12:21:00 -0500

    use consume() instead of i++ everywhere

--- demo.js
+++ demo.js
@@ -6,6 +6,7 @@ var Parser = require("./lib/parser.js");
 // var math = "a^b^c";
 var math = "a 2.5bold23(3-y(x-1))"
 // var math = "x^(-2)"
+// var math = "1 + 2 + -3 - 4 - -5";
 
 var parser = new Parser();
 var ast = parser.parse(math);
