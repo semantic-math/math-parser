a5833cacd2f2d9976a3d067aa16ab602e16b9d69
Kevin Barabash <kevinb@khanacademy.org>
2017-02-05 12:54:05 -0500

    stop using var

--- demo.js
+++ demo.js
@@ -1,15 +1,15 @@
-var Parser = require("./lib/parser.js");
+const Parser = require("./lib/parser.js");
 
-// var math = "1 + 2 * -(3 - 4)/(10*(x + y))";
-// var math = "x+y=1+2-3(4-x(2))";
-// var math = "ab/b/c*(d*e)";
-// var math = "a^b^c";
-var math = "a 2.5bold23(3-f(x-1))"
-// var math = "x^(-2)"
-// var math = "1 + 2 + -3 - 4 - -5";
+// const math = "1 + 2 * -(3 - 4)/(10*(x + y))";
+// const math = "x+y=1+2-3(4-x(2))";
+// const math = "ab/b/c*(d*e)";
+// const math = "a^b^c";
+const math = "a 2.5bold23(3-f(x-1))"
+// const math = "x^(-2)"
+// const math = "1 + 2 + -3 - 4 - -5";
 
-var parser = new Parser();
-var ast = parser.parse(math);
+const parser = new Parser();
+const ast = parser.parse(math);
 
 console.log(math);
 console.log(JSON.stringify(ast, null, 4));
