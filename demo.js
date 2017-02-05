4319108c5d8627dcebac99ca6a039fb80c711a1d
Kevin Barabash <kevinb@khanacademy.org>
2017-02-05 02:12:01 -0500

    initial commit

---
+++ demo.js
@@ -0,0 +1,14 @@
+var Parser = require("./lib/parser.js");
+
+// var math = "1 + 2 * -(3 - 4)/(10*(x + y))";
+// var math = "x+y=1+2-3(4-x(2))";
+// var math = "ab/b/c*(d*e)";
+// var math = "a^b^c";
+// var math = "a 2bold23(3-y(x-1))"
+var math = "a + (b+c)"
+
+var parser = new Parser();
+var ast = parser.parse(math);
+
+console.log(math);
+console.log(JSON.stringify(ast, null, 4));
