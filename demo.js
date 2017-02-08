e2bcace1fc7a9f02275a571047b0813baf2242a6
Kevin Barabash <kevinb@khanacademy.org>
2017-02-07 23:45:45 -0500

    Fixes #10, adds 'evaluate' function

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-const {Parser, print} = require("./build/bundle");
+const {evaluate, Parser, print} = require("./build/bundle");
 
 // const math = "1 + 2 * -(3 - 4)/(10*(x + y))";
 // const math = "x+y=1+2-3(4-x(2))";
@@ -12,7 +12,13 @@ const math = "a 2.5atan2(3-f(x-1))"
 const parser = new Parser();
 const ast = parser.parse(math);
 
-console.log(`original:    ${math}`);
-console.log(`printed :    ${print(ast)}`);
+console.log(`original :    ${math}`);
+console.log(`printed  :    ${print(ast)}`);
+
+const expr = "0.5 + atan2(1, 1) + 2 * 3 - 4 / 5";
+const result = evaluate(parser.parse(expr));
+
+console.log(`expr     :    ${expr}`);
+console.log(`result   :    ${result}`);
 
 // console.log(JSON.stringify(ast, null, 4));
