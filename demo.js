f86e1ef33a94f388c2fb2d8d464c58991d91e790
Kevin Barabash <kevinb@khanacademy.org>
2017-02-07 23:18:16 -0500

    add traverse and print functions

--- demo.js
+++ demo.js
@@ -1,16 +1,18 @@
-const Parser = require("./build/bundle").default;
+const {Parser, print} = require("./build/bundle");
 
 // const math = "1 + 2 * -(3 - 4)/(10*(x + y))";
 // const math = "x+y=1+2-3(4-x(2))";
 // const math = "ab/b/c*(d*e)";
 // const math = "a^b^c";
-// const math = "a 2.5atan2(3-f(x-1))"
+const math = "a 2.5atan2(3-f(x-1))"
 // const math = "x^(-2)"
 // const math = "1 + 2 - 3 + -4 - -5";
-const math = "--2";
+// const math = "--2";
 
 const parser = new Parser();
 const ast = parser.parse(math);
 
-console.log(math);
-console.log(JSON.stringify(ast, null, 4));
+console.log(`original:    ${math}`);
+console.log(`printed :    ${print(ast)}`);
+
+// console.log(JSON.stringify(ast, null, 4));
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     context: path.join(__dirname, "lib"),
-    entry: "./parser.js",
+    entry: "./index.js",
     output: {
         path: path.join(__dirname, "build"),
         filename: "bundle.js",
