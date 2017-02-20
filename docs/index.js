023f6454e21727a7c07479049547475645f26136
Kevin Barabash <kevinb@khanacademy.org>
2017-02-19 20:31:22 -0500

    add dropdown to change output format

--- docs/index.js
+++ docs/index.js
@@ -1,19 +1,35 @@
 const Parser = module.exports.Parser;
+const transformMathJS = module.exports.transformMathJS;
 
 const input = document.querySelector('#input');
 const output = document.querySelector('#output');
+const select = document.querySelector('#asttype');
+
+select.addEventListener('change', () => {
+    asttype = select.value;
+    update();
+})
 
 const params = new URLSearchParams(location.search);
 
+let asttype = 'mast-ast';
+
 if (params.has('math')) {
     input.value = params.get('math');
+}
+if (params.has('ast')) {
+    asttype = params.get('ast');
+    select.value = asttype;
 }
 
 const parser = new Parser();
 
 const update = () => {
     try {
-        const ast = parser.parse(input.value);
+        let ast = parser.parse(input.value);
+        if (asttype === 'mathsteps') {
+            ast = transformMathJS(ast);
+        }
         output.textContent = JSON.stringify(ast, null, 4);
     } catch (e) {
         output.textContent = e.message;
@@ -27,5 +43,5 @@ input.addEventListener('input', update);
 const permalink = document.querySelector('#permalink');
 permalink.addEventListener('click', () => {
     const math = encodeURI(input.value).replace(/\+/g, '%2B');
-    window.location = `?math=${math}`;
+    window.location = `?math=${math}&ast=${asttype}`;
 });
