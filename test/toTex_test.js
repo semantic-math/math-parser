9eaf0baf637796758c988376045f5b3b057b1baf
Anthony <aliang8@stuy.edu>
2017-04-29 21:49:05 -0400

    fixed toTex function

--- test/toTex_test.js
+++ test/toTex_test.js
@@ -5,16 +5,23 @@ import toTex from '../lib/toTex.js'
 
 describe("toTex", () => {
   it("handles wasMinus correctly", () => {
-    assert.equal(toTex(parse('1 - 2')), '1 - 2');
+    // arithmetic
+    assert.equal(toTex(parse('1 + -2')), '1 + -2');
     assert.equal(toTex(parse('1 - -2')), '1 - -2');
     assert.equal(toTex(parse('a - b')), 'a - b');
     assert.equal(toTex(parse('a + -b')), 'a + -b');
+    assert.equal(toTex(parse('1 * 2')), '1 ast 2');
+    // implicit multiplication
+    assert.equal(toTex(parse('x * 2x')), 'x ast 2 x'); 
+    assert.equal(toTex(parse('-3')), '-3');
+    // brackets
+    //assert.equal(toTex(parse('[2 , 3]')), '');
   });
 
-  // TODO(kevinb) enable these tests after updating division parsing behavior
-  it.skip("handles fractions correctly", () => {
-    assert.equal(toTex(parse('1/2/3')), '1 / 2 / 3');
-    assert.equal(parse('1*2/3'), '1 * (2 / 3)');
-    // assert.equal(toTex(parser.parse('(1*2)/3')), '(1 * 2) / 3');
+  it("handles fractions correctly", () => {
+    assert.equal(toTex(parse('1/2')), '\\frac{1}{2}');
+    assert.equal(toTex(parse('1/2/3')), '\\frac{\\frac{1}{2}}{3}');
+    assert.equal(toTex(parse('x/2')), '\\frac{x}{2}');
+    assert.equal(toTex(parse('(x+2)/(x+3)')), '\\frac{\\left(x + 2\\right)}{\\left(x + 3\\right)}');
   });
 });
