d20b95860df930f1211833bc7d748f08d0e24f9c
Kevin Barabash <kevinb@khanacademy.org>
2017-05-14 00:20:02 -0400

    remove nodes, bump version to 0.6.0

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import evaluate from './lib/evaluate'
-import * as nodes from './lib/nodes'
 import parse from './lib/parse'
 import print from './lib/print'
 
 export {
     evaluate,
-    nodes,
     parse,
     print,
 };
